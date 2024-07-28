a709ce7b2447e31cc6ee457fd82ac1d2ac969de8
2j2h5 <chawun36@gmail.com>
2024-07-28 19:27:18 +0900

    Train LeNet5 with GPU

--- LeNet5/train_LeNet5.py
+++ LeNet5/train_LeNet5.py
@@ -5,10 +5,6 @@ import torch.optim as optim
 from torchvision.datasets.mnist import MNIST
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
-import visdom
-import onnx
-
-viz = visdom.Visdom()
 
 data_train = MNIST('.',
                    train=True,
@@ -22,86 +18,47 @@ data_test = MNIST('.',
                   transform=transforms.Compose([
                       transforms.Resize((32, 32)),
                       transforms.ToTensor()]))
-data_train_loader = DataLoader(data_train, batch_size=256, shuffle=True, num_workers=8)
-data_test_loader = DataLoader(data_test, batch_size=1024, num_workers=8)
+data_train_loader = DataLoader(data_train, batch_size=256, shuffle=True)
+data_test_loader = DataLoader(data_test, batch_size=1024, shuffle=False)
 
 net = LeNet5()
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(net.parameters(), lr=2e-3)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 net.to(device)
 
-cur_batch_win = None
-cur_batch_win_opts = {
-    'title': 'Epoch Loss Trace',
-    'xlabel': 'Batch Number',
-    'ylabel': 'Loss',
-    'width': 1200,
-    'height': 600,
-}
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
 
-
-def train(epoch):
-    global cur_batch_win
+def train(net, device, data_train_loader, optimizer, epoch):
     net.train()
-    loss_list, batch_list = [], []
-    for i, (images, labels) in enumerate(data_train_loader):
-
+    for batch_index, (images, labels) in enumerate(data_train_loader):
         images, labels = images.to(device), labels.to(device)
         optimizer.zero_grad()
 
         output = net(images)
 
         loss = criterion(output, labels)
-
-        loss_list.append(loss.detach().cpu().item())
-        batch_list.append(i+1)
-
-        if i % 10 == 0:
-            print('Train Epoch: %d, Batch: %d, Loss: %f' % (epoch, i, loss.detach().cpu().item()))
-
-        # Update Visualization
-        if viz.check_connection():
-            cur_batch_win = viz.line(torch.Tensor(loss_list), torch.Tensor(batch_list),
-                                     win=cur_batch_win, name='current_batch_loss',
-                                     update=(None if cur_batch_win is None else 'replace'),
-                                     opts=cur_batch_win_opts)
-
         loss.backward()
         optimizer.step()
 
+        if batch_index % 100 == 0:
+            print(f'Train Epoch: {epoch}, [{batch_index * len(images)}/{len(data_train_loader.dataset)} ({100. * batch_index / len(data_train_loader):.0f}%)]\tLoss: {loss.item():.6f}')
 
-def test():
+def test(net, device, data_test_loader):
     net.eval()
     total_correct = 0
-    avg_loss = 0.0
-    for i, (images, labels) in enumerate(data_test_loader):
-        images, labels = images.to(device), labels.to(device)
-        output = net(images)
-        avg_loss += criterion(output, labels).sum()
-        pred = output.detach().max(1)[1]
-        total_correct += pred.eq(labels.view_as(pred)).sum()
+    test_loss = 0.0
+    with torch.no_grad():
+        for images, labels in data_test_loader:
+            images, labels = images.to(device), labels.to(device)
+            output = net(images)
+            test_loss += criterion(output, labels).item()
+            pred = output.argmax(dim=1, keepdim=True)
+            total_correct += pred.eq(labels.view_as(pred)).sum().item()
 
-    avg_loss /= len(data_test)
-    print('Test Avg. Loss: %f, Accuracy: %f' % (avg_loss.detach().cpu().item(), float(total_correct) / len(data_test)))
+    test_loss /= len(data_test_loader.dataset)
+    print(f'\nTest set: Average loss: {test_loss:.4f}, Accuracy: {total_correct}/{len(data_test_loader.dataset)} ({100. * total_correct / len(data_test_loader.dataset):.0f}%)\n')
 
-
-def train_and_test(epoch):
-    train(epoch)
-    test()
-
-    dummy_input = torch.randn(1, 1, 32, 32, requires_grad=True)
-    torch.onnx.export(net, dummy_input, "lenet.onnx")
-
-    onnx_model = onnx.load("lenet.onnx")
-    onnx.checker.check_model(onnx_model)
-
-
-def main():
-    for e in range(1, 16):
-        train_and_test(e)
-
-
-if __name__ == '__main__':
-    main()
+for epoch in range(1, 21):
+    train(net, device, data_train_loader, optimizer, epoch)
+    test(net, device, data_test_loader)
--- multi/train_convnet_multi.py
+++ multi/train_convnet_multi.py
@@ -29,10 +29,14 @@ def train(model, device, train_loader, optimizer, epoch):
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
+
         output = model(data)
+
         loss = criterion(output, target)
+
         loss.backward()
         optimizer.step()
+        
         if batch_idx % 100 == 0:
             print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)} ({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.item():.6f}')
 
